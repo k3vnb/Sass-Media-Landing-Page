b077777aac8476b1024da5ed6dac3ca967875fe0
Kevin Boyle <papershack@gmail.com>
2018-05-15 23:31:42 -0700

    change some styling, add new image, set preliminary script to show image

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+
+// this runs the loading icon
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function(){
   overlay.style.display = 'none';
 })
+// end of loading icon
+function showWalrus(){
+  alert("hello");
+}
+const walrusLink = document.querySelector(".walrus-link");
+
+
+walrusLink.addEventListener("click", showWalrus, false);
