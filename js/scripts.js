e77ec5137a23abff3915dc25089e18f7bfd5d6d2
Kevin Boyle <papershack@gmail.com>
2018-05-15 23:51:14 -0700

    add function for removing image overlay on click

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,18 @@ window.addEventListener('load', function(){
   overlay.style.display = 'none';
 })
 // end of loading icon
-function showWalrus(){
+function showWalrus(e){
+  e.preventDefault();
   let showTheWalrus = document.querySelector(".walrus-div");
   showTheWalrus.style.display = "flex";
 }
+function hideWalrus(){
+  let hideTheWalrus = document.querySelector(".walrus-div");
+  hideTheWalrus.style.display = "none";
+}
 const walrusLink = document.querySelector(".walrus-link");
+const walrusDiv = document.querySelector(".walrus-div");
 
 
 walrusLink.addEventListener("click", showWalrus);
+walrusDiv.addEventListener("click", hideWalrus);
